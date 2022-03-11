e5203e64b801768d70d4ce243d2e23737af8b26d
gabrielkulp <kulpga@oregonstate.edu>
2022-03-11 12:30:11 -0800

    Skip RAM columns, catch errors when testing

--- run-tests.py
+++ run-tests.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
+from subprocess import CalledProcessError
 import gateware
 
 if  __name__ == "__main__":
 	all_coords = []
-	for x in range(1,25): # 1-24
+	for x in range(6,25): # 1-24
+		if x == 6 or x == 19: # RAM column
+			continue
 		for y in range(1,31): # 1-30
 			all_coords.append((x,y))
-	
-	with open("results.json", "w") as f:
+
+	with open("results.json", "a") as f:
 		f.write("{\n")
 		for coords, i in zip(all_coords, range(len(all_coords))):
 			key = " ".join(map(str, coords))
-			results = gateware.run(coords)
-			f.write(f"\t\"{key}\": [")
-			f.write(", ".join(map(str, results)))
-			if i == len(all_coords)-1:
-				f.write("]\n")
-			else:
-				f.write("],\n")
-			f.flush()
+			try:
+				results = gateware.run(coords)
+				f.write(f"\t\"{key}\": [")
+				f.write(", ".join(map(str, results)))
+				if i == len(all_coords)-1:
+					f.write("]\n")
+				else:
+					f.write("],\n")
+				f.flush()
+			except CalledProcessError:
+				print("Failed on", key)
 		f.write("}\n")
